362aa67a9f96f664099fddbd01f2502dacc04416
bizika_xw <bizika_xw@126.com>
2018-06-27 14:32:36 +0800

    初始化

--- src/test/java/com/tan/springbootlearning/SpringbootlearningApplicationTests.java
+++ src/test/java/com/tan/springbootlearning/SpringbootlearningApplicationTests.java
@@ -1,9 +1,7 @@
 package com.tan.springbootlearning;
 
-import com.tan.springbootlearning.service.RestTemplateBaseService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,16 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootlearningApplicationTests {
 
-    @Autowired
-    RestTemplateBaseService service;
-
     @Test
     public void contextLoads() {
         System.out.println("1111");
-    }
-    @Test
-    public void httpGetWithoutHeader() {
-        String url = "http://localhost:8080/api/user/base/tag/get?id=20";
-        service.httpGetWithoutHeader(url);
     }
 }
