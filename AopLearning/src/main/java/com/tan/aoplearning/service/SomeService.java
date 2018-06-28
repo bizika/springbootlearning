7ab3fcf93b99f1ef2ccf0137ed13a960fb303fd9
bizika_xw <bizika_xw@126.com>
2018-06-28 17:44:10 +0800

    初始化

---
+++ AopLearning/src/main/java/com/tan/aoplearning/service/SomeService.java
@@ -0,0 +1,37 @@
+package com.tan.aoplearning.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+/**
+ * @author 601091
+ * @date 2018/6/28
+ */
+@Service
+@Slf4j
+public class SomeService {
+    private Random random = new Random(System.currentTimeMillis());
+
+    public void someMethod() {
+        log.info("---SomeService: someMethod invoked---");
+        try {
+            // 模拟耗时任务
+            Thread.sleep(random.nextInt(5000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void someMethod1() {
+        log.info("---SomeService: someMethod invoked---");
+        try {
+            // 模拟耗时任务
+            Thread.sleep(random.nextInt(5000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
